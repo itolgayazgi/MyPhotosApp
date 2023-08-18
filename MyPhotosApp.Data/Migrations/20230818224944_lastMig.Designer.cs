62be6ef8bb6c6ce63d34c9c275a4769c8fdd967f
itolgayazgi <i.tolgayazgi@gmail.com>
2023-08-19 01:50:35 +0300

    finish with Migs

--- MyPhotosApp.Data/Migrations/20230818224944_lastMig.Designer.cs
+++ MyPhotosApp.Data/Migrations/20230818224944_lastMig.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyPhotosApp.Data.Contexts;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MyPhotosApp.Persistance.Migrations
 {
     [DbContext(typeof(MyPhotosDbContext))]
-    partial class MyPhotosDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230818224944_lastMig")]
+    partial class lastMig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MyPhotosApp.Data/Migrations/20230818224944_lastMig.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPhotosApp.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class lastMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Photos",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Photos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_UserId",
+                table: "Photos",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Users_UserId",
+                table: "Photos",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Users_UserId",
+                table: "Photos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Photos_UserId",
+                table: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Photos");
+        }
+    }
+}
